329c2fb511788468232e667f43754f83bb6fb8e2
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-07 18:24:20 +0200

    Complete flipping

--- hoeffding.py
+++ hoeffding.py
@@ -1,21 +1,46 @@
 import random
 
+# Helper function 1
 def flip():
-	''' flip one coic '''
+	''' flip one coin '''
 	return random.choice(['H', 'T'])
 
-def flip1000(numCoins=1000):
-	''' flip numCoins coins and return res per coin '''
-	res, aCoin = [], []
+def flipCoins(numCoins=1000):
+	''' flip numCoins coins 10 times each and return res per coin '''
+	res = []
 	for i in range(numCoins):
-		for j in range(10):
+		aCoin = []
+		for j in range(10):		# flip each coin 10 times
 			aCoin.append(flip())
 		res.append(aCoin)
 	return res
 
-def pick3(listOfLists):
-	c1 = listOfLists[0]
-	cRand = random.choice(listOfLists)
-	cMin = 
+# Helper function 2
+def countH(aCoin):
+	counter = 0
+	for i in range(len(aCoin)):
+		if aCoin[i] == 'H':
+			counter += 1
+	return counter
 
-	return (c1, cRand, cMin)
+# Helper function 3
+def minHeads(allCoins):
+	numHeads = 10 				# max num heads per coin
+	coinIndex = 0
+	for i in range(len(allCoins)):
+		freqOfH = countH(allCoins[i])
+		if freqOfH < numHeads:
+			numHeads = freqOfH
+			coinIndex = i
+	return coinIndex
+
+def pick3(allCoins):
+	c1 = allCoins[0]
+	cRand = random.choice(allCoins)
+	minIndex = minHeads(allCoins)
+	cMin = allCoins[minIndex]
+	nu1 = countH(c1) / float(len(c1))
+	nuRand = countH(cRand) / float(len(cRand))
+	nuMin = countH(cMin) / float(len(cMin))
+	return [(c1, nu1), (cRand, nuRand), (cMin, nuMin)]
+
