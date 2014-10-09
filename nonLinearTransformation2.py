8dad5d410f742d5fae627f840332932b5292e335
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-09 23:46:40 +0200

    Still on problem 9git stgit stgit st

--- nonLinearTransformation2.py
+++ nonLinearTransformation2.py
@@ -23,30 +23,23 @@ step2 = np.dot(step1, X.transpose())
 w = np.dot(step2, y)
 print w
 
-mis = []
-for i in range(100):
-	randPoint = random.choice(data)
-	Xrand, yRand = randPoint[:-1], randPoint[-1]
-	yEst = np.sign(np.dot(Xrand, w))
-	if yEst != yRand:
-		mis.append(1)
-
-print len(mis)/ float(100)
-
 choices = [[-1, -.05, .08, .13, 1.5, 1.5], [-1, -.05, .08, .13, 1.5, 15], 
 [-1, -.05, .08, .13, 15, 1.5], [-1, -1.5, .08, .13, .05, .05], 
 [-1, -.05, .08, 1.5, .15, .15]]
 
-for choice in choices:
-	mis = []
-	for i in range(100):
-		randPoint = random.choice(data)
-		Xrand, yRand = randPoint[:-1], randPoint[-1]
-
-		yEst = np.sign(np.dot(Xrand, choice))
-
+myG = []
+mis = [[], [], [], [], []]
+for i in range(1000):
+	randPoint = random.choice(data)
+	Xrand, yRand = randPoint[:-1], randPoint[-1]
+	gEst = np.sign(np.dot(Xrand, w))
+	if gEst != yRand:
+		myG.append(gEst)
+	for j in range(len(choices)):
+		yEst = np.sign(np.dot(Xrand, choices[j]))
 		if yEst != yRand:
-			mis.append(1)
+			mis[j].append(1)
 
-	print 'choice ', choice
-	print len(mis)/ float(100)
+print len(myG)/ float(1000)
+for alist in mis:
+	print len(alist)/ float(1000)
