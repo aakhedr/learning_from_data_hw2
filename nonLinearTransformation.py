69924630139c6283950193b368045568d60aec0f
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-10 23:06:27 +0200

    question5

--- nonLinearTransformation.py
+++ nonLinearTransformation.py
@@ -32,9 +32,7 @@ def addFeatures(dataSet):
 # Run experiement 1000 times
 Ein = []
 data, slope, intercept = buildDataSet(N=1000)
-[X, y] = extract(data)
-X = addIntercept(X)
-data = np.column_stack((X, y))
+data = addIntercept(data)
 
 # for i in range(1000):
 # 	# question8
@@ -45,17 +43,29 @@ data = np.column_stack((X, y))
 # print sum(Ein)/ float(len(Ein))
 
 # question9
-data = addFeatures(data)
-w = calculateWeights(data)
-myG = calculateError(w, data)
-print 'Ein by w found', myG
 
+data = addFeatures(data)
+data = simulateNoise(data=data, noise=.1)
 
 choices = [[-1, -.05, .08, .13, 1.5, 1.5], [-1, -.05, .08, .13, 1.5, 15], 
 [-1, -.05, .08, .13, 15, 1.5], [-1, -1.5, .08, .13, .05, .05], 
 [-1, -.05, .08, 1.5, .15, .15]]
 
-for j in range(len(choices)):
-	print 'Ein by choice ', j
-	print calculateError(choices[j], data)
+Ein_w = []
+Ein_a, Ein_b, Ein_c, Ein_d, Ein_e = [], [], [], [], []
+for i in range(1000):
+	w = calculateWeights(data)
+	Ein_w.append(calculateError(w, data))
 
+	Ein_a.append(calculateError(choices[0], data))
+	Ein_b.append(calculateError(choices[1], data))
+	Ein_c.append(calculateError(choices[2], data))
+	Ein_d.append(calculateError(choices[3], data))
+	Ein_e.append(calculateError(choices[4], data))
+
+print 'Ein_w', sum(Ein_w)/ float(len(Ein_w))
+print 'Ein_a', sum(Ein_a)/ float(len(Ein_a))
+print 'Ein_b', sum(Ein_b)/ float(len(Ein_b))
+print 'Ein_c', sum(Ein_c)/ float(len(Ein_c))
+print 'Ein_d', sum(Ein_d)/ float(len(Ein_d))
+print 'Ein_e', sum(Ein_e)/ float(len(Ein_e))
