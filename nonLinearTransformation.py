e6441ea62f3562fdd4167b8c47ca51848025820f
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-11 01:29:53 +0200

    All g choices are still similar and varying.

--- nonLinearTransformation.py
+++ nonLinearTransformation.py
@@ -29,13 +29,11 @@ def addFeatures(dataSet):
 
 	return dataSet
 
-# Run experiement 1000 times
-Ein = []
+# question8
 data, slope, intercept = buildDataSet(N=1000)
 data = addIntercept(data)
-
+# Ein = []
 # for i in range(1000):
-# 	# question8
 # 	data = simulateNoise(data=data, noise=.1)
 # 	w = calculateWeights(data)
 # 	Ein.append(calculateError(w, data))
@@ -43,29 +41,30 @@ data = addIntercept(data)
 # print sum(Ein)/ float(len(Ein))
 
 # question9
-
 data = addFeatures(data)
 data = simulateNoise(data=data, noise=.1)
 
 choices = [[-1, -.05, .08, .13, 1.5, 1.5], [-1, -.05, .08, .13, 1.5, 15], 
 [-1, -.05, .08, .13, 15, 1.5], [-1, -1.5, .08, .13, .05, .05], 
 [-1, -.05, .08, 1.5, .15, .15]]
+choices = [np.array(choices[i]) for i in range(len(choices))]
 
-Ein_w = []
-Ein_a, Ein_b, Ein_c, Ein_d, Ein_e = [], [], [], [], []
-for i in range(1000):
+Ein_w, Ein_a, Ein_b, Ein_c, Ein_d, Ein_e = [], [], [], [], [], []
+iters = 1
+for i in range(iters):
 	w = calculateWeights(data)
-	Ein_w.append(calculateError(w, data))
 
+	Ein_w.append(calculateError(w, data))
 	Ein_a.append(calculateError(choices[0], data))
 	Ein_b.append(calculateError(choices[1], data))
 	Ein_c.append(calculateError(choices[2], data))
 	Ein_d.append(calculateError(choices[3], data))
 	Ein_e.append(calculateError(choices[4], data))
 
-print 'Ein_w', sum(Ein_w)/ float(len(Ein_w))
-print 'Ein_a', sum(Ein_a)/ float(len(Ein_a))
-print 'Ein_b', sum(Ein_b)/ float(len(Ein_b))
-print 'Ein_c', sum(Ein_c)/ float(len(Ein_c))
-print 'Ein_d', sum(Ein_d)/ float(len(Ein_d))
-print 'Ein_e', sum(Ein_e)/ float(len(Ein_e))
+print 'Ein_w', sum(Ein_w)/ float(iters)
+print 'Ein_a', sum(Ein_a)/ float(iters)
+print 'Ein_b', sum(Ein_b)/ float(iters)
+print 'Ein_c', sum(Ein_c)/ float(iters)
+print 'Ein_d', sum(Ein_d)/ float(iters)
+print 'Ein_e', sum(Ein_e)/ float(iters)
+print
