f80945a20348e4f96536645b84ff2d4bee3770ad
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-11 13:03:11 +0200

    Still on problem9

--- nonLinearTransformation.py
+++ nonLinearTransformation.py
@@ -38,53 +38,56 @@ data = addIntercept(data)
 
 # print sum(Ein)/ float(len(Ein))
 
-# question9 and 10
+# question9 
 data = addFeatures(data)
 data = simulateNoise(data=data, noise=.1)
 
-# choices = [[-1, -.05, .08, .13, 1.5, 1.5], [-1, -.05, .08, .13, 1.5, 15], 
-# [-1, -.05, .08, .13, 15, 1.5], [-1, -1.5, .08, .13, .05, .05], 
-# [-1, -.05, .08, 1.5, .15, .15]]
-# choices = [np.array(choices[i]) for i in range(len(choices))]
+choices = [[-1, -.05, .08, .13, 1.5, 1.5], 
+[-1, -.05, .08, .13, 1.5, 15], 
+[-1, -.05, .08, .13, 15, 1.5], 
+[-1, -1.5, .08, .13, .05, .05], 
+[-1, -.05, .08, 1.5, .15, .15]]
+choices = [np.array(choices[i]) for i in range(len(choices))]
 
-# Ein_w, Ein_a, Ein_b, Ein_c, Ein_d, Ein_e = [], [], [], [], [], []
-# iters = 1
-# for i in range(iters):
-# 	w = calculateWeights(data)
+Ein_w, Ein_a, Ein_b, Ein_c, Ein_d, Ein_e = [], [], [], [], [], []
+iters = 1000
+for i in range(iters):
+	w = calculateWeights(data)
 
-# 	Ein_w.append(calculateError(w, data))
-# 	Ein_a.append(calculateError(choices[0], data))
-# 	Ein_b.append(calculateError(choices[1], data))
-# 	Ein_c.append(calculateError(choices[2], data))
-# 	Ein_d.append(calculateError(choices[3], data))
-# 	Ein_e.append(calculateError(choices[4], data))
+	Ein_w.append(calculateError(w, data))
+	Ein_a.append(calculateError(choices[0], data))
+	Ein_b.append(calculateError(choices[1], data))
+	Ein_c.append(calculateError(choices[2], data))
+	Ein_d.append(calculateError(choices[3], data))
+	Ein_e.append(calculateError(choices[4], data))
 
-# print 'Ein_w', sum(Ein_w)/ float(iters)
-# print 'Ein_a', sum(Ein_a)/ float(iters)
-# print 'Ein_b', sum(Ein_b)/ float(iters)
-# print 'Ein_c', sum(Ein_c)/ float(iters)
-# print 'Ein_d', sum(Ein_d)/ float(iters)
-# print 'Ein_e', sum(Ein_e)/ float(iters)
-# print
+print 'Ein_w', sum(Ein_w)/ float(iters)
+print
+print 'Ein_a', sum(Ein_a)/ float(iters)
+print 'Ein_b', sum(Ein_b)/ float(iters)
+print 'Ein_c', sum(Ein_c)/ float(iters)
+print 'Ein_d', sum(Ein_d)/ float(iters)
+print 'Ein_e', sum(Ein_e)/ float(iters)
 
-w = calculateWeights(data)
+# # problem10
+# w = calculateWeights(data)
 
-outOfSample = generateOutOfSamplePoints(1000, slope, intercept)
-outOfSample = addIntercept(outOfSample)
-# outOfSample = addFeatures(outOfSample)	# check function!
+# outOfSample = generateOutOfSamplePoints(1000, slope, intercept)
+# outOfSample = addIntercept(outOfSample)
+# # outOfSample = addFeatures(outOfSample)	# check function!
 
-X, y = extract(outOfSample)
-x3 = X[:, 1] * X[:, 2]
-x4 = X[:, 1]**2
-x5 = X[:, 2]**2
-outOfSample = np.column_stack((X, x3, x4, x5, y))
-outOfSample = simulateNoise(outOfSample)
+# X, y = extract(outOfSample)
+# x3 = X[:, 1] * X[:, 2]
+# x4 = X[:, 1]**2
+# x5 = X[:, 2]**2
+# outOfSample = np.column_stack((X, x3, x4, x5, y))
+# outOfSample = simulateNoise(outOfSample)
 
-Eout, Ein = [], []
-for i in range(1000):
-	Ein.append(calculateError(w, data))
-	Eout.append(calculateError(w, outOfSample))
+# Eout, Ein = [], []
+# for i in range(1000):
+# 	Ein.append(calculateError(w, data))
+# 	Eout.append(calculateError(w, outOfSample))
 
-print 'Ein ', sum(Ein)/ float(1000)
-print 'Eout ', sum(Eout)/ float(1000)
+# print 'Ein ', sum(Ein)/ float(1000)
+# print 'Eout ', sum(Eout)/ float(1000)
 
